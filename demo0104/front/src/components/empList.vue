98196b99e4e6eee87069eee68f0df3ff858a8ba0
yuri <rladbfld@naver.com>
2023-01-04 12:34:01 +0900

    vue init

---
+++ demo0104/front/src/components/empList.vue
@@ -0,0 +1,38 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>사원번호</th>
+                <th>이름</th>
+                <th>성</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="emp in empList" v-bind:key="emp.employeeId">
+                <td>{{ emp.employeeId }}</td>
+                <td>{{ emp.firstName }}</td>
+                <td>{{ emp.lastName }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+
+export default {
+    name: 'empList',
+    data: function(){
+        return {
+            empList : {}
+        }
+    },
+    created : function(){
+        fetch('http://localhost:8090/empInfoList')
+        .then(response => response.json())
+        .then(data => {
+            this.empList = data;
+        })
+        .catch(error => console.log(error));
+    }
+}
+</script>
--- demo0104/src/main/java/com/example/demo/emp/web/EmpController.java
+++ demo0104/src/main/java/com/example/demo/emp/web/EmpController.java
@@ -1,12 +1,16 @@
 package com.example.demo.emp.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.emp.mapper.EmpMapper;
+import com.example.demo.emp.service.EmpVO;
 
 @Controller
 public class EmpController {
@@ -19,5 +23,11 @@ public class EmpController {
 	public String empList(Model model) {
 		model.addAttribute("empList", empMapper.getEmpList(null));
 		return "empList";
+	}
+	
+	@RequestMapping(value = "/empInfoList", method = RequestMethod.GET)
+	@ResponseBody
+	public List<EmpVO> empInfoList(Model model){
+		return empMapper.getEmpList(null);
 	}
 }
